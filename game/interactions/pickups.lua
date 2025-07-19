a61dc7128127a4727504c1f552a3339634975917
Treecher <treechcer@gmail.com>
2025-07-19 21:20:30 +0200

    added sound for pick up

--- game/interactions/pickups.lua
+++ game/interactions/pickups.lua
@@ -5,12 +5,13 @@ local player = require("game.properties.player")
 local engine = require("game.renderNstore.engine")
 local inventory = require("game.renderNstore.inventory")
 local bilboarding = require("game.renderNstore.bilboarding")
+local audio = require("sounds.audio")
 
 function pickups.pickupWeapon()
     for key, value in pairs(map.gunMap) do
         --print (engine.calculateDistance(player.x, player.y, map.gunMap[key].tileX * map.block2DSize, map.gunMap[key].tileY * map.block2DSize))
-        print(key)
-        if engine.calculateDistance(player.x, player.y, map.gunMap[key].tileX * map.block2DSize, map.gunMap[key].tileY * map.block2DSize) <= 50 then
+        --print(key)
+        if engine.calculateDistance(player.x, player.y, map.gunMap[key].tileX * map.block2DSize, map.gunMap[key].tileY * map.block2DSize) <= 75 then
             if inventory.gunSlots[inventory.equipedGunSlot] == "" then
                 inventory.gunSlots[inventory.equipedGunSlot] = key
 
@@ -24,6 +25,10 @@ function pickups.pickupWeapon()
                 table.remove(bilboarding, i)
                 --table.remove(map.gunMap, index)
                 map.gunMap[key] = nil
+
+                audio.pickup:setPitch(1 - math.random() / 2)
+                audio.pickup:play()
+
                 break
             end
         end
--- sounds/audio.lua
+++ sounds/audio.lua
@@ -3,7 +3,8 @@
 local settings = require("UINreletad.settings")
 
 audio = {
-    blockBreak = love.audio.newSource("sounds/effects/blockBreak.wav", "static")
+    blockBreak = love.audio.newSource("sounds/effects/blockBreak.wav", "static"),
+    pickup = love.audio.newSource("sounds/effects/pickup.wav", "static"),
 }
 
 audio.blockBreak:setVolume(settings.audio.effects)
